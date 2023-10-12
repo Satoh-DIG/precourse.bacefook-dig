a6789fc9fdb1a1dd9002b0e5fb6361233d47c1a8
Satoh-DIG <124141304+Satoh-DIG@users.noreply.github.com>
2023-10-12 11:33:47 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// windowのイベントとしてクリックを作ると、ウィンドウ内のランダムな場所でのクリックで反応してしまう
-// window.addEventListener("click",function bClick() {alert("メッセージ");})
+// friendColors 配列をグローバルスコープで定義
+const friendColors = ["red", "blue", "green", "pink", "yellow", "purple"];
 
 // load　ページの読み込みが完了した時のイベント発生
 window.addEventListener("load", () => {
   // This is a check to see if there's a username stored
   let username = localStorage.getItem("username");
-  // １回目だけの理由がわからん　２度目にはダイヤログなし
   if (!username) {
     // Prompt for one if a username isn't found
     username = window.prompt("What is your name?");
@@ -14,13 +13,17 @@ window.addEventListener("load", () => {
 
   const containerEl = document.querySelector("#newsfeed");
 
-
   // ------------------------------------------------------------
   // HTMLFormElement オブジェクトを作成する
   // ------------------------------------------------------------
+  // formタグを準備する
   let form = document.createElement("form");
-  // BODY のノードリストに登録する
-  document.body.appendChild(form);
+  // h1タグのノードを取得する
+  let h1Tag = document.getElementsByTagName("h1");
+  // formタグをh1タグの後に挿入する
+  h1Tag[0].after(form);
+
+  // document.body.appendChild(form);
   // ------------------------------------------------------------
   // HTMLInputElement オブジェクトを作成する
   // ------------------------------------------------------------
@@ -32,7 +35,7 @@ window.addEventListener("load", () => {
 
   // 変数userTextに、userInput内のinnerText内のテキストを格納する
   let userText = document.getElementById("userInput").value;
-
+  console.log(userText);
   // ボタン作成
   // ボタン要素を作成
   let btn = document.createElement("button");
@@ -41,10 +44,10 @@ window.addEventListener("load", () => {
   btn.id = "btnId";
   // ボタンを追加 （コメント欄のフォームに追加する）
   form.appendChild(btn);
-  
-
-
-  // 書き込むためのループ？
+  //**********************************************
+  //初期ループ
+  //**********************************************
+  // newsfeedを書き込むためのループ
   // This makes things appear
   for (let index = bacefook.newsfeed.length - 1; index >= 0; index--) {
     const post = bacefook.newsfeed[index];
@@ -54,56 +57,84 @@ window.addEventListener("load", () => {
     friendEl.innerText = post.friend;
 
     const postEl = document.createElement("div");
+    postEl.className = "current-info";
     postEl.innerText = post.text;
     postEl.append(friendEl);
 
-    const momentTime = moment().format("MMMM Do YYYY, h:mm:ss a");
-    containerEl.append(momentTime);
+    // 友達ごとに色を設定
+    console.log(bacefook.friendNames.indexOf())
+    console.log(friendColors[index])
+    if (friendColors[index]) {
+      friendEl.style.border = `3px solid ${friendColors[index]}`;
+    } else {
+      friendEl.style.border = "3px solid #ccc"; // デフォルトの色
+    }
+
+    const momentTime = moment(bacefook.newsfeed[index].timestamp).format(
+      "MMMM Do YYYY, h:mm:ss a"
+    );
+    postEl.append(momentTime);
+    // console.log(bacefook.newsfeed[index].timestamp)
+
+    postEl.append(bacefook.newsfeed[index].image);
+    // console.log(bacefook.newsfeed[index]);
+    // 改行を追加
+    const brElement = document.createElement("br");
+    postEl.append(brElement);
 
     containerEl.append(postEl);
-    containerEl.append(bacefook.newsfeed[index].image)
-    console.log(bacefook.newsfeed[index]) 
   }
-  let currentCountFeed = bacefook.newsfeed.length;
-    // ボタンクリックアクション
-    function bClick(event){
+  //**********************************************
+  //ボタンループ
+  //**********************************************
+  // let currentCountFeed = bacefook.newsfeed.length;
+  // ボタンクリックアクション
+  function bClick(event) {
+    let currentInfo = document.getElementsByClassName("current-info");
 
-      //let currentInfo = document.getElementsByClassName("current-info");
-      //console.log(currentInfo.value);
-      //currentInfo.remove();
-
-      for (let index = bacefook.newsfeed.length - 1; index >= currentCountFeed; index--) {
-        const post = bacefook.newsfeed[index];
-    
-        const friendEl = document.createElement("div");
-        friendEl.className = "friend";
-        friendEl.innerText = post.friend;
-    
-        const postEl = document.createElement("div");
-        postEl.innerText = post.text;
-        postEl.prepend(friendEl);
-
-        const momentTime=moment().format('MMMM Do YYYY, h:mm:ss a');
-        containerEl.prepend(momentTime);
-        
-        containerEl.prepend(postEl);   
-      
-         
-        // containerEl.prepend(basefook.newsfeed.image)
-      }
-      //alert(input.value);
-      // console.log(input.value);
-      event.preventDefault();
+    for (let j = currentInfo.length - 1; j >= 0; j--) {
+      // consol.log(currentInfo.value);
+      currentInfo[j].remove();
     }
-      
+    console.log(bacefook.newsfeed.length);
+    for (let index = bacefook.newsfeed.length - 1; index >= 0; index--) {
+      console.log("BTN");
+      const post = bacefook.newsfeed[index];
 
-  const Button = document.getElementById('btnId');
-  Button.addEventListener('click', bClick);
+      const friendEl = document.createElement("div");
+      friendEl.className = "friend";
+      friendEl.innerText = post.friend;
 
+      const postEl = document.createElement("div");
+      postEl.className = "current-info";
+      postEl.innerText = post.text;
+      postEl.append(friendEl);
 
+      // 友達ごとに色を設定
+      // console.log(friendColors[index])
+      if (friendColors[index]) {
+        friendEl.style.border = `3px solid ${friendColors[index]}`;
+      } else {
+        friendEl.style.border = "3px solid #ccc"; // デフォルトの色
+      }
 
+      const momentTime = moment(bacefook.newsfeed[index].timestamp).format(
+        "MMMM Do YYYY, h:mm:ss a"
+      );
+      postEl.append(momentTime);
+
+      postEl.append(bacefook.newsfeed[index].image);
+
+      const brElement = document.createElement("br");
+      postEl.append(brElement);
+
+      containerEl.append(postEl);
+    }
+    //alert(input.value);
+    // console.log(input.value);
+    event.preventDefault();
+  }
+
+  const Button = document.getElementById("btnId");
+  Button.addEventListener("click", bClick);
 });
-
-
-
-
--- dataGenerator.js
+++ dataGenerator.js
@@ -5,199 +5,212 @@
   but please check it out to familiarize yourself beforehand.
 */
 (() => {
-  window.bacefook = {};
-  bacefook.newsfeed = [];
-  bacefook.friends = {};
-  bacefook.friendNames = ["tamaroh", "kani", "eriko", "tsubasa", "masataka"];
-  bacefook.friendNames.forEach(name => {
-    bacefook.friends[name] = [];
-  });
+    window.bacefook = {};
+    bacefook.newsfeed = [];
+    bacefook.friends = {};
+    bacefook.friendNames = ["tamaroh", "kani", "eriko", "tsubasa", "masataka"];
+    bacefook.friendNames.forEach(name => {
+      bacefook.friends[name] = [];
+    });
 
-  const getRandomElement = array => {
-    // Given an array, returns a random element
-    const randomIndex = Math.floor(Math.random() * array.length);
-    return array[randomIndex];
-  };
-
-  const starters = [
-    "totally just",
-    "just",
-    "completely",
-    "waaaaah! i",
-    "i just",
-    "a salaryman",
-    "a salaryman",
-    "yesterday I",
-    "a ninja",
-    "my boss"
-  ];
-  const verbs = [
-    "ate",
-    "drank",
-    "threw up in",
-    "refactored",
-    "iterated on",
-    "thought about",
-    "threw up on",
-    "saw",
-    "walked to",
-    "got lost in",
-    "walked into",
-    "googled",
-    "drove",
-    "ran to",
-    "worked on",
-    "slept on",
-    "slept in"
-  ];
-  const fillers = [
-    "my",
-    "your",
-    "his",
-    "her",
-    "my favorite",
-    "a beautiful",
-    "a delicious",
-    "that",
-    "this",
-    "an interesting",
-    "",
-    "the best",
-    "the greatest",
-    "a delightful"
-  ];
-  const nouns = [
-    "DIG",
-    "restaurant",
-    "omakase",
-    "hitomedia",
-    "family mart",
-    "private jet",
-    "mama",
-    "lawsons",
-    "conbini",
-    "whisky",
-    "onigiri",
-    "car",
-    "food",
-    "house",
-    "toilet",
-    "tokyo",
-    "city",
-    "iphone",
-    "google",
-    "unicorn",
-    "mess",
-    "pirate ship",
-    "ninja"
-  ];
-  const hashtags = [
-    "#DIG",
-    "#techlife",
-    "#toyota",
-    "#tokyo",
-    "#japan",
-    "#interesting",
-    "#til",
-    "#lol",
-    "#tgifriday",
-    "#hashtags",
-    "#japanlife",
-    "#oops",
-    ""
-  ];
-  const feelings = [
-    "happy",
-    "smug",
-    "lovestruck",
-    "gross",
-    "scared",
-    "tired",
-    "angry",
-    "frustrated",
-    "excited",
-    ""
-  ];
-  const images = [
-    // "😃",
-    // "😭",
-    {
-      src:"images/m59991772593_1.jpg",
-      alt:"image1"
-    },
-    {
-      src:"images/sick_rougan_smartphone.png",
-      alt:"image2"
-    },
-    {
-      src:"images/kaigi_shinken_women.png",
-      alt:"image3"
-    }
-  ];
-
-  const generateRandomText = () => {
-    return [
-      getRandomElement(starters),
-      getRandomElement(verbs),
-      getRandomElement(fillers),
-      getRandomElement(nouns),
-      getRandomElement(hashtags),
-      // getRandomElement(feelings),
-      // getRandomElement(images),
-    ].join(" ");
-  };
-
-  function pictFactory(obj) {
-    let result = document.createElement("img");
-    // console.log(result)
-    result.src = obj["src"]; // 画像パス
-    result.alt = obj["alt"]; // 代替テキスト
-    return result;
-    // let content_area = document.getElementById("content_area");
-    // content_area.appendChild(result);
-  }
-
-
-  const generatePostObj = timeOffset => {
-    // if an offset is provided, make the timestamp that much older,
-    // otherwise just use the current time
-    const timestamp = timeOffset
-      ? new Date(new Date().getTime() - timeOffset)
-      : new Date();
-
-      // console.log(pictFactory(getRandomElement(images)))
-    return {
-      friend: getRandomElement(bacefook.friendNames),
-      text: generateRandomText(),
-      feeling: getRandomElement(feelings),
-      image: pictFactory(getRandomElement(images)),
-      timestamp
+    
+  
+    const getRandomElement = array => {
+      // Given an array, returns a random element
+      const randomIndex = Math.floor(Math.random() * array.length);
+      return array[randomIndex];
     };
-  };
-
-  const addPost = obj => {
-    const friend = obj.friend;
-    bacefook.friends[friend].push(obj);
-    bacefook.newsfeed.push(obj);
-  };
-
-  const createPost = timeOffset => {
-    const newPost = generatePostObj(timeOffset);
-    // console.log(newPost)
-    addPost(newPost);
-  };
-
-  for (let i = 0; i < 10; i++) {
-    // make the starting posts look like they were posted over the course of the past day
-    const timeOffset = (2 * (10 - i) + Math.random()) * 60 * 60 * 1000;
-    createPost(timeOffset);
-  }
-
-  const scheduler = () => {
-    createPost(null);
-    setTimeout(scheduler, (3 + Math.random() * 5) * 1000); 
-    // generate a new post every 3 to 8 seconds
-  };
-
-  scheduler();
-})();
+  
+    const starters = [
+      "totally just",
+      "just",
+      "completely",
+      "waaaaah! i",
+      "i just",
+      "a salaryman",
+      "a salaryman",
+      "yesterday I",
+      "a ninja",
+      "my boss"
+    ];
+    const verbs = [
+      "ate",
+      "drank",
+      "threw up in",
+      "refactored",
+      "iterated on",
+      "thought about",
+      "threw up on",
+      "saw",
+      "walked to",
+      "got lost in",
+      "walked into",
+      "googled",
+      "drove",
+      "ran to",
+      "worked on",
+      "slept on",
+      "slept in"
+    ];
+    const fillers = [
+      "my",
+      "your",
+      "his",
+      "her",
+      "my favorite",
+      "a beautiful",
+      "a delicious",
+      "that",
+      "this",
+      "an interesting",
+      "",
+      "the best",
+      "the greatest",
+      "a delightful"
+    ];
+    const nouns = [
+      "DIG",
+      "restaurant",
+      "omakase",
+      "hitomedia",
+      "family mart",
+      "private jet",
+      "mama",
+      "lawsons",
+      "conbini",
+      "whisky",
+      "onigiri",
+      "car",
+      "food",
+      "house",
+      "toilet",
+      "tokyo",
+      "city",
+      "iphone",
+      "google",
+      "unicorn",
+      "mess",
+      "pirate ship",
+      "ninja"
+    ];
+    const hashtags = [
+      "#DIG",
+      "#techlife",
+      "#toyota",
+      "#tokyo",
+      "#japan",
+      "#interesting",
+      "#til",
+      "#lol",
+      "#tgifriday",
+      "#hashtags",
+      "#japanlife",
+      "#oops",
+      ""
+    ];
+    const feelings = [
+      "happy",
+      "smug",
+      "lovestruck",
+      "gross",
+      "scared",
+      "tired",
+      "angry",
+      "frustrated",
+      "excited",
+      ""
+    ];
+    const images = [
+      // "😃",
+      // "😭",
+      {
+        src:"images/m59991772593_1.jpg",
+        alt:"image1",
+        width:"200"
+      },
+      {
+        src:"images/sick_rougan_smartphone.png",
+        alt:"image2",
+        width:"200"
+      },
+      {
+        src:"images/job_chocolatiere_woman.png",
+        alt:"image2",
+        width:"200"
+      },
+      {
+        src:"images/kaigi_shinken_women.png",
+        alt:"image3",
+        width:"200"
+      }
+    ];
+  
+    const generateRandomText = () => {
+      return [
+        getRandomElement(starters),
+        getRandomElement(verbs),
+        getRandomElement(fillers),
+        getRandomElement(nouns),
+        getRandomElement(hashtags),
+        // getRandomElement(feelings),
+        // getRandomElement(images),
+      ].join(" ");
+    };
+  
+    function pictFactory(obj) {
+      let result = document.createElement("img");
+      // console.log(result)
+      result.src = obj["src"]; // 画像パス
+      result.alt = obj["alt"]; // 代替テキスト
+      result.width = obj["width"]; // 画像の幅
+      return result;
+      // let content_area = document.getElementById("content_area");
+      // content_area.appendChild(result);
+    }
+  
+  
+    const generatePostObj = timeOffset => {
+      // if an offset is provided, make the timestamp that much older,
+      // otherwise just use the current time
+      const timestamp = timeOffset
+        ? new Date(new Date().getTime() - timeOffset)
+        : new Date();
+  
+        // console.log(pictFactory(getRandomElement(images)))
+      return {
+        friend: getRandomElement(bacefook.friendNames),
+        text: generateRandomText(),
+        feeling: getRandomElement(feelings),
+        image: pictFactory(getRandomElement(images)),
+        timestamp
+      };
+    };
+  
+    const addPost = obj => {
+      const friend = obj.friend;
+      bacefook.friends[friend].push(obj);
+      bacefook.newsfeed.push(obj);
+    };
+  
+    const createPost = timeOffset => {
+      const newPost = generatePostObj(timeOffset);
+      // console.log(newPost)
+      addPost(newPost);
+    };
+  
+    for (let i = 0; i < 10; i++) {
+      // make the starting posts look like they were posted over the course of the past day
+      const timeOffset = (2 * (10 - i) + Math.random()) * 60 * 60 * 1000;
+      createPost(timeOffset);
+    };
+  
+    const scheduler = () => {
+      createPost(null);
+      setTimeout(scheduler, (3 + Math.random() * 5) * 1000); 
+      // generate a new post every 3 to 8 seconds
+      // console.log("scheduler")
+      console.log(bacefook.newsfeed);
+    };
+    console.log("scheduler")
+    scheduler();
+  })();
